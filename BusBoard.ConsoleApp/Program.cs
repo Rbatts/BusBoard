3990be937f2d6a91ed0720eba764eecfc06510ff
Rbatts <32482377+Rbatts@users.noreply.github.com>
2017-10-12 16:08:45 +0100

    BusBoard now lets users input a postcode

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -10,54 +10,105 @@ using System.Net;
 
 namespace BusBoard.ConsoleApp
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
+        static void Main(string[] args)
+        {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
-            var client = new RestClient();
-            client.BaseUrl = new Uri("https://api.tfl.gov.uk/");
-            client.Authenticator = new HttpBasicAuthenticator("2e2a0c23", "dc17c694c0041fabbe7d31d366ddde94");
+            var tflApi = new TflApi();
+            var postcodeApi = new PostCodeApi();
 
             Console.Write("Enter your postcode.");
-            string uResponse = Console.ReadLine();
-
-                var request = new RestRequest();
-                request.Resource = $"StopPoint/{uResponse}/Arrivals"; //string interperation ${} places variables within strings.
-
-                var response = client.Execute(request);
-                var JSONFromApi = response.Content;
-            try
+            string postcode = Console.ReadLine();
+            PostcodeLocation location = postcodeApi.GetLongLat(postcode);
+            string stopcode = tflApi.GetStopCodeNearLocation(location.longitude, location.latitude);
+            var nextBusses = tflApi.GetNextArrivalsForStop(stopcode);
+            List<Bus> bussesSorted = nextBusses.OrderBy(b => Convert.ToInt32(b.TimeToStation)).ToList();
+            List<Bus> bussesFirstFive = bussesSorted.GetRange(0, 5);
+            foreach (var Bus in bussesFirstFive)
             {
-                var busses = JsonConvert.DeserializeObject<List<Bus>>(JSONFromApi);
-                List<Bus> bussesSorted = busses.OrderBy(b => Convert.ToInt32(b.TimeToStation)).ToList();
-                List<Bus> bussesFirstFive = bussesSorted.GetRange(0, 5);
-                foreach (var Bus in bussesFirstFive)
-                {
-                    int minutes = Bus.TimeToStation / 60;
-                    Console.WriteLine(Bus.Towards + " " + minutes + " " + Bus.DestinationName);
+                int minutes = Bus.TimeToStation / 60;
+                Console.WriteLine(Bus.Towards + " " + minutes + " " + Bus.DestinationName);
+            }
 
-                }
-            }
-            catch
-            {
-                Console.WriteLine("You have not entered an invalid bus stop code.");
-                Console.ReadLine();
-            }
-            Console.ReadLine();   
+            Console.ReadLine();
+        }
     }
-  }
     class Bus
     {
         public int TimeToStation { get; set; }
-       
+
         public string Towards { get; set; }
-      
+
         public string DestinationName { get; set; }
 
     }
+    class TflApi
+    {
+        public RestClient restClient = new RestClient();
+        public RestRequest restRequest = new RestRequest();
+        public TflApi()
+        {
+            restClient.BaseUrl = new Uri("https://api.tfl.gov.uk/");
+            restClient.Authenticator = new HttpBasicAuthenticator("2e2a0c23", "dc17c694c0041fabbe7d31d366ddde94");
+        }
+        public List<Bus> GetNextArrivalsForStop(string stopCode)
+        {
+            restRequest.Resource = $"StopPoint/{stopCode}/Arrivals"; //string interperation ${} places variables within strings.
+            var response = restClient.Execute<List<Bus>>(restRequest);
+            var arrivals = response.Data;
+            return arrivals;
+        }
+        public string GetStopCodeNearLocation(float longitude, float latitude)
+        {
+            restRequest.Resource = $"StopPoint?stopTypes=NaptanPublicBusCoachTram&lat={latitude}&lon={longitude}";
+            IRestResponse<StopPointDetails> response = restClient.Execute<StopPointDetails>(restRequest);
+            StopPointDetails stopPointDetails = response.Data;
+            StopPoint FirstStopPoint = stopPointDetails.stopPoints[0];
+            string stopcode = FirstStopPoint.naptanId;
+            return stopcode;
+        }
 
+    }
+    class PostCodeApi
+    {
+        public RestClient restClient = new RestClient();
+        public RestRequest restRequest = new RestRequest();
+
+        public PostCodeApi()
+        {
+            restClient.BaseUrl = new Uri("https://api.postcodes.io/");
+        }
+        public PostcodeLocation GetLongLat(string postcode)
+        {
+            restRequest.Resource = $"postcodes/{postcode}";
+            var response = restClient.Execute<PostcodeDetails>(restRequest);
+            var location = response.Data.result;
+            return location;
+        }
+
+    }
+
+    class PostcodeDetails
+    {
+        public PostcodeLocation result { get; set; }
+    }
+
+    class PostcodeLocation
+    {
+        public float longitude { get; set; }
+        public float latitude { get; set; }
+    }
+    class StopPointDetails
+    {
+        public List<StopPoint> stopPoints { get; set; }
+    }
+
+    class StopPoint
+    {
+        public string naptanId { get; set; }
+    }
 }
 
 
